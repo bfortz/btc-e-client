19d387c8be77d08d1b7ac88758ff5298f270af74
Bernard Fortz <bfortz@ulb.ac.be>
2017-09-18 09:17:34 +0200

    Version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject io.curtis/btc-e-client "0.3.0"
-  :description "Small btc-e client library"
+(defproject io.curtis/btc-e-client "0.4.0"
+  :description "Small btc-e / wex client library"
   :url "https://github.com/MarcoPolo/btc-e-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.2"] 
-                 [http-kit "2.1.10"]
-                 [pandect "0.3.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"] 
+                 [http-kit "2.2.0"]
+                 [pandect "0.6.1"]])
